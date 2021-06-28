14b1f76f37f55bb0f046e9652cf303b0bcab5a4d
Javier Torres <javierandrestorresreyes@gmail.com>
2021-06-28 15:58:15 -0500

    modulos

--- other/template.cpp
+++ other/template.cpp
@@ -12,8 +12,12 @@ typedef pair<ll,ll> ii;
 #define foreach(it, l) for (auto it = l.begin(); it != l.end(); it++)
 #define fore(i, a, b) for (int i = a, almo5t = b; i < almo5t; ++i)
 #define SZ(x) ((int)x.size())
-#define MOD 1000000007
 #define FIN ios::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+
+const int MOD=1e9+7;
+int add(int a, int b){a+=b;if(a>=MOD)a-=MOD;return a;}
+int sub(int a, int b){a-=b;if(a<0)a+=MOD;return a;}
+int mul(ll a, ll b){return a*b%MOD;}
 
 int main()
 { FIN;
